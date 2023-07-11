1475ab54b3b8f54407f24884ce5ec965e4ca344e
IhorMarch <97352675+IhorMarch@users.noreply.github.com>
2023-07-11 22:49:44 +0300

    fixed 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,20 +2,28 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
+
 const bodyEl = document.querySelector("body")
 const btnStart= document.querySelector("button[data-start]")
 const btnStop = document.querySelector("button[data-stop]")
+btnStop.disabled = true;
 let timerId = null;
+
 
 
 btnStart.addEventListener("click", () => {
   timerId = setInterval(() => {
     const color = getRandomHexColor()
-      bodyEl.style.backgroundColor = color;;
-      btnStart.disabled = true;
+    bodyEl.style.backgroundColor = color;;
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+  
   }, 1000);});
 
 btnStop.addEventListener("click", () => {
   clearInterval(timerId);
   btnStart.disabled = false;
+  btnStop.disabled = true;
 });
+
+
