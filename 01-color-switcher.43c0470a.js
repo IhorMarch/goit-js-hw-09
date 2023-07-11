3845c44e9f235b7445b9835400d82c1c0ed2a3c1
IhorMarch <97352675+IhorMarch@users.noreply.github.com>
2023-07-11 19:50:10 +0000

    Deploying to gh-pages from @ IhorMarch/goit-js-hw-09@1475ab54b3b8f54407f24884ce5ec965e4ca344e 🚀

---
+++ 01-color-switcher.43c0470a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0;let a=null;e.addEventListener("click",(()=>{a=setInterval((()=>{const a=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=a,e.disabled=!0,d.disabled=!1}),1e3)})),d.addEventListener("click",(()=>{clearInterval(a),e.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.43c0470a.js.map
